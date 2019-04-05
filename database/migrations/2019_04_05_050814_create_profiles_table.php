19fec13cbad788451940d9575f31b6d50adbad7a
erickmx <erickblanc96@gmail.com>
2019-04-05 00:23:06 -0600

    creada migracion de la tabla de profiles
    closes: #3

---
+++ database/migrations/2019_04_05_050814_create_profiles_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('picture_url');
+            $table->string('first_name', 50);
+            $table->string('last_name', 50);
+            $table->tinyInteger('age');
+            $table->integer('user_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+}
