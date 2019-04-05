f6cdb102c308f0d046803c5d90e5e769247bf63f
erickmx <erickblanc96@gmail.com>
2019-04-05 11:01:37 -0600

    Creada migracion de la tabla de publicaciones, falta agregar la relacion con category y clasification #4

---
+++ database/migrations/2019_04_05_164103_create_publications_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePublicationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('publications', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title', 200);
+            $table->timestamp('creation_date')->useCurrent();
+            $table->text('content');
+            $table->text('urlCover');
+            $table->integer('user_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('publications');
+    }
+}
