f810146616e243169bbdff9b9f11cca24575f948
erickmx <erickblanc96@gmail.com>
2019-04-29 16:56:34 -0500

    Terminadas de crear las migraciones de las tablas para la base de datos:
    - commentaries
    - categories
    - classifications
    - savedPosts
    
    closes: #7
    closes: #8
    closes: #9
    closes: #37

---
+++ database/migrations/2019_04_29_215400_create_publications_saved_users_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePublicationsSavedUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('publications_saved_users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->integer('publication_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+
+            $table->foreign('publication_id')
+                ->references('id')
+                ->on('publications')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('publications_saved_users');
+    }
+}
