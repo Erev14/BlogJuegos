67df35eb7cd22d5eaa9bc4885462257fabcc2639
Erev14 <erevjoach14@gmail.com>
2019-05-28 01:06:20 -0500

    add softDeletes

--- app/Commentary.php
+++ app/Commentary.php
@@ -3,12 +3,14 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Commentary extends Model
 {
+    use SoftDeletes;
     /**
      * The attributes that are mass assignable.
-     * 
+     *
      * @var array
      */
     protected $fillable = [
--- app/Publication.php
+++ app/Publication.php
@@ -3,12 +3,14 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Publication extends Model
 {
+    use SoftDeletes;
     /**
      * The attributes that are mass assignable.
-     * 
+     *
      * @var array
      */
     protected $fillable = [
